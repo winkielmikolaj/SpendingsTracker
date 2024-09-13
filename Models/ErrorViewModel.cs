b04825814ade9cd5eb7ea08967e5a2e94beda5d9
Mikołaj <pzx112240@student.poznan.merito.pl>
2024-09-13 07:18:06 +0200

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpendingsTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Expense.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendingsTracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public double Value { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+    }
+}
